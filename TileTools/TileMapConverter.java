b9cf57a25d2ad20b25a7a27864030bf4884abd90
kaqui-null <emanuelgomes24.08@gmail.com>
2024-08-30 14:21:24 -0300

    Planned TileMapConverter

--- TileTools/TileMapConverter.java
+++ TileTools/TileMapConverter.java
@@ -1,4 +1,19 @@
 package TileTools;
 
+import java.awt.*;
+import java.io.File;
+
 public class TileMapConverter {
+
+    private File tileTxt;
+
+    public TileMapConverter(String tileSetLocation) {
+        tileTxt = new File(tileSetLocation);
+    }
+
+    /*
+    * TODO: Send converter info to panel creator
+    *       Use info in Cell class to create cells
+    *       Add cells to panel*/
+
 }
